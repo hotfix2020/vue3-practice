be67cd731154933ad074950b36c89714fc5dc4e7
徐旺 <ljzsyww@foxmail.com>
2021-06-10 17:06:38 +0800

    feat:测试

--- src/views/other.vue
+++ src/views/other.vue
@@ -1,9 +1,23 @@
 <template>
-  <h1>other</h1>
+  <h1>{{ count }}</h1>
+  <el-buttom @click="plus">plus</el-buttom>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue'
+
 export default {
-  setup() {}
+  setup() {
+    const count = ref(0)
+
+    function plus() {
+      count.value += 1
+    }
+
+    return {
+      count,
+      plus
+    }
+  }
 }
 </script>
